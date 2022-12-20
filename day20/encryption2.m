c453e448673eaa537032a5d6a17cfd836cf3c4a3
Teemu Härkönen <teemu.harkonen@lut.fi>
2022-12-20 20:01:25 +0200

    Day 20

---
+++ day20/encryption2.m
@@ -0,0 +1,93 @@
+
+clc
+clear
+close all
+
+signal = readmatrix("input.txt");
+
+dkey = 811589153;
+signal = signal(:)';
+signal = dkey * signal;
+
+nData = length( signal );
+shifted = signal;
+
+inds = 1:nData;
+
+format long g
+
+times = 10;
+
+for t = 1:times
+    for ii = 1:nData
+        
+        shift_ii = signal(ii);
+        shift_ii = rem( shift_ii, nData - 1);
+        ind_ii = find( ii == inds );
+    
+        newInd_ii = ind_ii + shift_ii;
+    
+        if( shift_ii == 0 )
+            continue;
+        else
+            newInd_ii = ind_ii + shift_ii;
+    
+            if( newInd_ii == 1 )
+                newInd_ii = nData;
+            end
+    
+            if( newInd_ii <= 0 )
+                newInd_ii = nData + newInd_ii - 1;
+            end
+    
+            if( newInd_ii > nData )
+                newInd_ii = newInd_ii - nData + 1;
+            end
+        end
+    
+        if( newInd_ii > ind_ii )
+    
+            leftStart = ind_ii;
+            leftStop = newInd_ii - 1;
+        
+            rightStart = ind_ii + 1;
+            rightStop = newInd_ii;
+    
+            leftInds = leftStart:leftStop;
+            rightInds = rightStart:rightStop;
+    
+            inds( leftInds ) = inds( rightInds );
+        else
+    
+            leftStart = newInd_ii;
+            leftStop = ind_ii - 1;
+        
+            rightStart = newInd_ii + 1;
+            rightStop = ind_ii;
+    
+            leftInds = leftStart:leftStop;
+            rightInds = rightStart:rightStop;
+    
+            inds( rightInds ) = inds( leftInds );
+        end
+    
+        inds( newInd_ii ) = ii;
+    end
+end
+
+shiftedSignal = signal( inds );
+zeroInd = find( shiftedSignal == 0 );
+shiftedSignal = circshift( shiftedSignal, -zeroInd);
+
+num1000 = circshift( shiftedSignal, -999);
+num1000 = num1000(1)
+
+num2000 = circshift( shiftedSignal, -1999);
+num2000 = num2000(1)
+
+num3000 = circshift( shiftedSignal, -2999);
+num3000 = num3000(1)
+
+total = num1000 + num2000 + num3000
+
+format default
