8c6d203bb7ce74193b7a53ae214394d934ed0680
Teemu Härkönen <teemu.harkonen@lut.fi>
2022-12-25 16:41:39 +0200

    Day 24

--- day24/blizzardNavigation.m
+++ day24/blizzardNavigation.m
@@ -3,29 +3,160 @@ clc
 clear
 close all
 
-data = readlines("test.txt")
+data = readlines("input.txt");
 
-[ map, blizzards] = constructMap( data )
-character = makeCharacter( map );
+[ map, blizzards] = constructMap( data );
+[ maxII, maxJJ] = size( map );
 
-time = 0;
-while( ~targetReached )
+start = [ 1, 2];
+target = [ maxII, maxJJ - 1];
+visited = zeros( maxII, maxJJ, 300);
+
+tic
+[ timePart1, blizzards] = getDistance( start, target, blizzards, map, visited, 1);
+[ timeMid,   blizzards] = getDistance( target, start, blizzards, map, visited, timePart1 + 1);
+[ timePart2, blizzards] = getDistance( start, target, blizzards, map, visited, timeMid + 1);
+toc
+
+part1 = num2str( timePart1 );
+part2 = num2str( timePart2 );
+
+disp("Part 1: " + part1);
+disp("Part 2: " + part2);
+
+function [time, blizzards] = getDistance( start, target, blizzards, map, visited, time)
     
-    time = getDistance( character, blizzards, time + 1);
+
+    period = size( visited, 3);
+    [ maxII, maxJJ] = size( map );
+    timeIndexNext = mod( time, period) + 1;
+
+    visited_t = visited( :, :, timeIndexNext);
+    blizzards = updateBlizzards( blizzards, map);
+
+    possibleLocations = getPossibleLocations( start, map, blizzards, visited_t);
+    possibleII = possibleLocations(:,1);
+    possibleJJ = possibleLocations(:,2);
+
+    possibleInds = sub2ind( [ maxII, maxJJ], possibleII, possibleJJ);
+
+    visitedNext = visited( :, :, timeIndexNext);
+    visitedNext( possibleInds ) = 1;
+    visited( :, :, timeIndexNext) = visitedNext;
+
+    includesTarget = ismember( target, possibleLocations, "rows");
+    includesTarget = any( includesTarget );
+
+    pathsNext = possibleLocations;
+    
+    while( ~includesTarget )
+
+        timeIndexNext = mod( time + 1, period) + 1;
+        blizzards = updateBlizzards( blizzards, map);
+
+        time = time + 1;
+        disp("Current time: " + num2str(time));
+        
+        paths = pathsNext;
+        pathsNext = [];
+
+        nPaths = size( paths, 1);
+        
+        for ii = 1:nPaths
+
+            visited_t = visited( :, :, timeIndexNext);
+            
+            path_ii = paths( ii, :);
+            paths_ii = getPossibleLocations( path_ii, map, blizzards, visited_t);
+            
+            includesTarget = ismember( target, paths_ii, "rows");
+            includesTarget = any( includesTarget );        
+            
+            if( ~includesTarget )
+                
+                visitedII = paths_ii(:,1);
+                visitedJJ = paths_ii(:,2);
+            
+                newVisitedInds = sub2ind( [ maxII, maxJJ], visitedII, visitedJJ);
+            
+                visitedNext = visited( :, :, timeIndexNext);
+                visitedNext( newVisitedInds ) = 1;
+                visited( :, :, timeIndexNext) = visitedNext;
+
+                pathsNext = [ pathsNext; paths_ii];
+            else
+                break;
+            end
+        end
+    end
 end
 
-function [distance] = getDistance( character, blizzards, time)
+function [possibleLocations] = getPossibleLocations( p, map, blizzards, visited)
+
+    [ maxII, maxJJ] = size( map );
+
+    kernel = [ 0, 1;
+               1, 0;
+               0, 0;
+              -1, 0;
+               0,-1];
+
+    wallInds = find( map == 1 );
+    visitedInds = find( visited == 1 );
+
+    [ wallII, wallJJ] = ind2sub( [ maxII, maxJJ], wallInds);
+    [ visitedII, visitedJJ] = ind2sub( [ maxII, maxJJ], visitedInds);
+
+    north = blizzards.north;
+    east = blizzards.east;
+    south = blizzards.south;
+    west = blizzards.west;
+
+    possibleLocations = p + kernel;
+    outOfBoundsII = possibleLocations(:,1) < 1 | possibleLocations(:,1) > maxII;
+    outOfBoundsJJ =  possibleLocations(:,2) < 1 | possibleLocations(:,2) > maxJJ;
+    outOfBounds = outOfBoundsII | outOfBoundsJJ;
+
+    wallLocations = [ wallII, wallJJ];
+    visitedLocations = [ visitedII, visitedJJ];
+    blizzardLocations = [north; east; south; west];
     
-    
+    inds = ~outOfBounds;
+    inds = inds & ~ismember( possibleLocations, blizzardLocations, "rows");
+    inds = inds & ~ismember( possibleLocations, wallLocations, "rows");
+    inds = inds & ~ismember( possibleLocations, visitedLocations, "rows");
+
+    possibleLocations = possibleLocations( inds, :);
 end
 
-function [character] = makeCharacter( map )
+function [blizzards] = updateBlizzards( blizzards, map)
 
-    [ mapHeight, mapWidth] = size( map );
+    [ maxII, maxJJ] = size( map );
 
-    character = {};
-    character.position = [ 1, 2];
-    character.target = [ mapHeight, mapWidth - 1];
+    north = blizzards.north;
+    east = blizzards.east;
+    south = blizzards.south;
+    west = blizzards.west;
+
+    north = north + [-1, 0];
+    east = east + [ 0 1];
+    south = south + [1 0];
+    west = west + [ 0 -1];
+
+    northBounds = north(:,1) == 1;
+    eastBounds = east(:,2) == maxJJ;
+    southBounds = south(:,1) == maxII;
+    westBounds = west(:,2) == 1;
+
+    north( northBounds, 1) = maxII - 1;
+    east( eastBounds, 2) = 2;
+    south( southBounds, 1) = 2;
+    west( westBounds, 2) = maxJJ - 1;
+
+    blizzards.north = north;
+    blizzards.east = east;
+    blizzards.south = south;
+    blizzards.west = west;
 end
 
 function [ map, blizzards] = constructMap( lines )
@@ -45,16 +176,16 @@ function [ map, blizzards] = constructMap( lines )
     
     for ii = 1:mapHeight
         
-        line_ii = lines(ii)
+        line_ii = lines(ii);
         line_ii = char( line_ii );
         
-        wallInds = line_ii == '#'
+        wallInds = line_ii == '#';
         map( ii, wallInds) = 1;
         
-        northInds = find( line_ii == '^' )
-        eastInds = find( line_ii == '>' )
-        southInds = find( line_ii == 'v' )
-        westInds = find( line_ii == '<' )
+        northInds = find( line_ii == '^' );
+        eastInds = find( line_ii == '>' );
+        southInds = find( line_ii == 'v' );
+        westInds = find( line_ii == '<' );
         
         northInds = northInds(:);
         eastInds = eastInds(:);
@@ -82,5 +213,3 @@ function [ map, blizzards] = constructMap( lines )
         blizzards.west = [ blizzards.west; blizzardsWest];    
     end
 end
-
-
